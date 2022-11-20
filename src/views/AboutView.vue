04752dda5a78f52f0c82bd26d22b5d09965915c7
Oblasov Ilya <oblasovs@icloud.com>
2022-11-20 22:51:15 +0300

    fix report

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="layout" style="justify-content: center;">
+  <div class="layout">
     <span>До конференции осталось</span>
     <div class="home">
       <TimerComponent />
     </div>
     <a href="#report" style="text-decoration: underline; text-align: center;">Ознакомиться<br>с повесткой</a>
   </div>
-  <div class="layout" id="report" style="justify-content:center; padding-top: 56px;">
+  <div class="layout" id="report" style="padding-top: 66px;">
     <span  style="color:#ff8b00; font-size: 3em; font-weight: 700;">Повестка</span>
     <ul>
       <li>Отчет Профкома студентов</li>
-      <li style="align-self:flex-end">Отчет контрольно-ревизионной комиссии Профкома студентов</li>
+      <li >Отчет контрольно-ревизионной комиссии Профкома студентов</li>
       <li>Выборы председателя Профкома студентов</li>
-      <li style="align-self:flex-end">Выборы членов Профкома студентов</li>
+      <li >Выборы членов Профкома студентов</li>
       <li>Выборы контрольно-ревизионной комиссии Профкома студентов</li>
-      <li style="align-self:flex-end">Выборы делегатов на Профсоюзную конференцию Профкома МГУ</li>
+      <li >Выборы делегатов на Профсоюзную конференцию Профкома МГУ</li>
       <li>Разное</li>
     </ul>
   </div>
@@ -36,7 +36,7 @@ export default {
   flex-direction: column;
   align-items: center;
   padding: 1em;
-  gap: 8vw;
+  justify-content: space-evenly;
   min-height: 90vh;
 }
 
@@ -63,8 +63,8 @@ li{
   color: white;
   font-size: 1.1em;
   font-weight: 400;
-  list-style-type: none;
-  width: 70%;
+  list-style-type: decimal;
+  width: 90%;
 
 }
 </style>
